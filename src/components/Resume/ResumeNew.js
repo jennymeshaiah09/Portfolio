41f2a3b948d6c0faa64d50da718887df47207924
Jenny Meshaiah <katikijenny@gmail.com>
2023-03-14 10:16:51 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -22,12 +22,6 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
